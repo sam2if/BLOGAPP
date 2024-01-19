012e24fdc42fa5ddd85c9a8120f9132a35d2bb0b
Samuel <samif3443@gmail.com>
2024-01-19 17:16:17 +0300

    Adding test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :comments, foreign_key: 'post_id'
-  has_many :likes, foreign_key: 'post_id'
-
-  def update_user_posts_counter
-    author.update(posts_counter: author.posts.count)
-  end
-
-  def recent_comments(limit = 5)
-    comments.order(created_at: :desc).limit(limit)
-  end
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 end
